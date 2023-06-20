42ea5550a5468a769ebb4fc7e38ab11afc4650d7
GuangyinLu <94053085+GuangyinLu@users.noreply.github.com>
2023-06-20 09:44:46 -0400

    Delete poeme3 directory

--- poeme3/poeme3.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-print("La vie est belle.")
-print("La vie est un cadeau.")
-
-print("je dois revoir la vidéo pour bien conanaitre le GitHub!")
--- poeme3/poeme3de3.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-#poeme de python test
-print("Nous avons pleins des options, mais seulement quelques uns qui marchent :,)")
